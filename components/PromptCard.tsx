0f9f0632f4338626cecb00569873efbc483c5d5b
kaiowsz <kaionpsa@gmail.com>
2023-08-01 19:46:23 -0300

    endpoints of editing prompts

--- components/PromptCard.tsx
+++ components/PromptCard.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useState } from "react"
 import Image from "next/image"
@@ -7,6 +7,11 @@ import { usePathname, useRouter } from "next/navigation"
 
 
 const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}: any) => {
+
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
 
   const [copied, setCopied] = useState<string>("")
 
@@ -39,6 +44,13 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}: any) => {
 
       <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
       <p className="font-inter text-sm blue_gradient cursor-pointer" onClick={() => handleTagClick && handleTagClick(post.tag)}>{post.tag}</p>
+
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p className="font-inter text-sm green_gradient cursor_pointer" onClick={handleEdit}>Edit</p>
+          <p className="font-inter text-sm orange_gradient cursor_pointer" onClick={handleDelete}>Delete</p>
+        </div>
+      )}
     </div>
   )
 }
