fc0695268cc14d946253b87f752dee1c2d65cde2
kaiowsz <kaionpsa@gmail.com>
2023-08-02 17:04:30 -0300

    delete and edit prompts

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,14 +2,15 @@
 
 import React, { useState, useEffect } from "react"
 import { useSession } from "next-auth/react"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 
 import Profile from "@/components/Profile"
 
 const ProfilePage = () => {
 
-    const {data: session } = useSession()
+    const { data: session } = useSession()
     const [posts, setPosts] = useState([])
+    const router = useRouter()
 
     useEffect(() => {
         const fetchPosts = async() => {
@@ -25,8 +26,25 @@ const ProfilePage = () => {
 
     }, [])
 
-    const handleEdit = () => {}
-    const handleDelete = async () => {}
+    const handleEdit = (post: any) => {
+        router.push(`/update-prompt?id=${post._id}`)
+    }
+
+    const handleDelete = async (post: any) => {
+        const hasConfirmed = confirm("Are you sure you want to delete this prompt?")
+
+        if(hasConfirmed) {
+            try {
+                await fetch(`/api/prompt/${post._id.toString()}`, { method: "DELETE" })
+
+                const filteredPosts = posts.filter((currentPost: any) => currentPost._id !==  post._id)
+
+                setPosts(filteredPosts)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    }
 
     return (
         <Profile name="My" desc="Welcome to your personalized profile page" data={posts} handleEdit={handleEdit} handleDelete={handleDelete} />
