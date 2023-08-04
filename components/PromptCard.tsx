e5efc591e551a482b8ff608772becf33c288a27e
kaiowsz <kaionpsa@gmail.com>
2023-08-04 20:07:26 -0300

    feature: see other's profile

--- components/PromptCard.tsx
+++ components/PromptCard.tsx
@@ -21,15 +21,19 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}: any) => {
     setTimeout(() => setCopied(""), 3000)
   }
 
+  const handleProfileView = () => {
+    router.push(`/profile?id=${post.creator._id}`)
+  }
+
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer" onClick={handleProfileView}>
           <Image src={post.creator.image} alt="user_image" width={40} height={40} className="rounded-full object-contain" />
 
           <div className="flex flex-col">
             <h3 className="font-satoshi font-semibold text-gray-900">{post.creator.username}</h3>
-            <p className="font-inter text-sm text-gray-500">{post.creator.email}</p>
+            {/* <p className="font-inter text-sm text-gray-500">{post.creator.email}</p> */}
           </div>
         </div>
 
